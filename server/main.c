e7b06aad50d651cb8467aa6eeaef76753bab19f4
gazzol_j <gazzol_j@epitech.eu>
2015-04-27 11:46:23 +0200

    Makefile serveur

---
+++ server/main.c
@@ -0,0 +1,14 @@
+/*
+** main.c for main_serveur in /home/gazzol_j/rendu/PSU_2014_zappy/server
+** 
+** Made by julien gazzola
+** Login   <gazzol_j@epitech.net>
+** 
+** Started on  Mon Apr 27 11:43:52 2015 julien gazzola
+** Last update Mon Apr 27 11:44:00 2015 julien gazzola
+*/
+
+int	main()
+{
+  return (0);
+}
