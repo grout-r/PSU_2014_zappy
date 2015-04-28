cf90ba2912f22b9d043f50784b5adf32c45cb92b
gottin_o <gottin_o@epitech.eu>
2015-04-28 18:48:46 +0200

    [oscar] Modifs on Makefile

--- server/main.c
+++ server/main.c
@@ -1,14 +1,14 @@
 /*
-** main.c for main_serveur in /home/gazzol_j/rendu/PSU_2014_zappy/server
+** main.c for zappy in /home/oscar/rendu/PSU_2014_zappy/server
 ** 
-** Made by julien gazzola
-** Login   <gazzol_j@epitech.net>
+** Made by Oscar Morizet
+** Login   <oscar@epitech.net>
 ** 
-** Started on  Mon Apr 27 11:43:52 2015 julien gazzola
-** Last update Mon Apr 27 11:44:00 2015 julien gazzola
+** Started on  Tue Apr 28 18:48:21 2015 Oscar Morizet
+** Last update Tue Apr 28 18:48:24 2015 Oscar Morizet
 */
 
-int	main()
+int			main()
 {
   return (0);
 }
