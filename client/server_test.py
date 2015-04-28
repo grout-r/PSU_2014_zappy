5c05db4dc1bc0c45832c03c2ee8cce9e5950ea2c
Cedric Voinnet <cedric.voinnet@epitech.eu>
2015-04-28 14:11:19 +0200

    Modified test server for python

--- client/server_test.py
+++ client/server_test.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python           
 
-import socket               # Import socket module
+import socket
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
+s = socket.socket()
+host = "127.0.0.1"
+port = 12345
+s.bind((host, port))
 
 print 'host: {} port: {}'.format(host, port)
-s.listen(5)                 # Now wait for client connection.
+s.listen(5)
 while True:
-   c, addr = s.accept()     # Establish connection with client.
+   c, addr = s.accept()
    print 'Got connection from', addr
    c.send('Thank you for connecting')
-   c.close()                # Close the connection
+   c.close()
